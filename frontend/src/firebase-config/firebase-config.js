83f7a168d32b75bc645942f189b088ca0548d4cb
RadoslavCvetanov <radoslav.a.tsvetanov.2021@elsys-bg.org>
2023-03-21 21:42:17 +0200

    added firebase initialization

---
+++ frontend/src/firebase-config/firebase-config.js
@@ -0,0 +1,20 @@
+import firebase from "firebase";
+const firebaseConfig = {
+  apiKey: "AIzaSyBW1qk43LMOvCv5EuChXSCA5p0fTPyc868",
+
+  authDomain: "tuesfest.firebaseapp.com",
+
+  projectId: "tuesfest",
+
+  storageBucket: "tuesfest.appspot.com",
+
+  messagingSenderId: "574059060032",
+
+  appId: "1:574059060032:web:82944ab8b220e2fbbef8a5",
+
+  measurementId: "G-QPKCW6GM4D",
+};
+
+// Initialize Firebase
+
+const app = firebase.initializeApp(firebaseConfig);
