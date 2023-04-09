57d8003c419d7e87b4a61eab532f7c851d30e860
RadoslavCvetanov <radoslav.a.tsvetanov.2021@elsys-bg.org>
2023-04-09 16:51:53 +0300

    auth for doctor in chat

--- frontend/src/Components/Chat.jsx
+++ frontend/src/Components/Chat.jsx
@@ -1,17 +1,31 @@
-import React, { useContext } from "react";
+import React, { useContext ,useEffect} from "react";
 import Messages from "./Messages";
 import Input from "./Input";
 import { ChatContext } from "../context/chat-context-firebase";
 import UserDocuments from "./UserDocuments";
+import { authDoctorFromMongo } from "../doc-auth/auth-doc";
+import { AuthContext } from "../context/auth-context-firebase-trash";
 const Chat = () => {
   const { data } = useContext(ChatContext);
-
+  const [isDoc,setDoc] = React.useState(false)
+  const {currentUser} = useContext(AuthContext)
+  useEffect(() => {
+    async function auth (){
+      const doc = await authDoctorFromMongo("http://localhost:5000/api/docs",currentUser.displayName);
+      console.log("--------doc")
+    console.log(doc)
+    if(doc.data.length > 0){
+      setDoc(true);
+    }
+    }
+    auth();
+  })
   return (
     <div className="chat">
       <div className="chatInfo">
         <span>{data.user?.displayName}</span>
         <div className="chatIcons">
-          <UserDocuments name = {data.user?.displayName}/>
+          {isDoc && <UserDocuments name = {data.user?.displayName}/>}
         </div>
       </div>
       <Messages />
