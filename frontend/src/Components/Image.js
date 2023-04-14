a90e1c47ae262ebc4be0dd041ab95b5d3f9752b4
Borimir <borimir.ganchev@gmail.com>
2023-04-14 11:20:52 +0300

    changed the scrollTo behaviour

--- frontend/src/Components/Image.js
+++ frontend/src/Components/Image.js
@@ -1,15 +1,15 @@
 import React from "react";
 
 const Image = () => {
-    const handleClick = () =>{
-        window.scrollTo(0, 870);
-      }
+    const handleClick = () => {
+        window.scrollTo({top: 870, behavior: 'smooth'});
+    };
     return(
         <div className="Image">
             <img className="image" src="/Pictures/doctor.jpg" alt="Doctor"></img>
             <div className="mainTextContainer">
                 <h2 className="bigText">Consult your doctor and find treatment</h2>
-                <p className="smallText">Тhis site is aimed at people who want to consult their personal doctors, or want to find treatment for a given disease</p>
+                <p className="smallText">This site is aimed at people who want to consult their personal doctors, or want to find treatment for a given disease</p>
             </div>
             <div className="arrow">
                 <button className="arrowButton" onClick={handleClick}>
@@ -17,7 +17,7 @@ const Image = () => {
                 </button>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default Image
+export default Image;
